b7401848c38ebdffce291f46f7374307a51c58ee
radekaadek <radekaadek@gmail.com>
2024-10-11 16:44:00 +0200

    rename

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import networkx as nx
 import os
 import time
 
-kzh_lat, kzh_lon = 8.5686, 76.8731
-mdcl_lat, mdcl_lon = 8.52202892500963, 76.926448394559
+source_lat, source_lon = 8.5686, 76.8731
+dest_lat, dest_lon = 8.52202892500963, 76.926448394559
 
 if not os.path.exists('graph.xml'):
     print("Downloading graph")
-    north = max(kzh_lat, mdcl_lat)
-    south = min(kzh_lat, mdcl_lat)
-    east = max(kzh_lon, mdcl_lon)
-    west = min(kzh_lon, mdcl_lon)
+    north = max(source_lat, dest_lat)
+    south = min(source_lat, dest_lat)
+    east = max(source_lon, dest_lon)
+    west = min(source_lon, dest_lon)
     G = ox.graph.graph_from_bbox(bbox=(north, south, east, west),
                                  network_type='drive')
     ox.io.save_graph_xml(G, 'graph.xml')
@@ -25,18 +25,18 @@ print(f"Number of edges: {len(G.edges)}")
 
 
 # fetch the nearest node w.r.t coordinates
-orig = ox.distance.nearest_nodes(G, kzh_lon, kzh_lat)
-dest = ox.distance.nearest_nodes(G, mdcl_lon, mdcl_lat)
+orig = ox.distance.nearest_nodes(G, source_lon, source_lat)
+dest = ox.distance.nearest_nodes(G, dest_lon, dest_lat)
 
 def heur(n1, n2):
     n1x = G.nodes[n1]['x']
     n1y = G.nodes[n1]['y']
     n2x = G.nodes[n2]['x']
     n2y = G.nodes[n2]['y']
-    sourcex = mdcl_lon
-    sourcey = mdcl_lat
-    destx = kzh_lon
-    desty = kzh_lat
+    sourcex = source_lon
+    sourcey = source_lat
+    destx = dest_lon
+    desty = dest_lat
     return ((n1x - sourcex)**2 + (n1y - sourcey)**2 + (n2x - destx)**2 + (n2y - desty)**2)**0.5
 
 
