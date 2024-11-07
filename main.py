ecf9d38311e8359a9c8a584996c1019b2ee9c560
magzsko <maksskorupowski@gmail.com>
2024-11-07 23:34:46 +0100

    fix heur travel time

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ import matplotlib.pyplot as plt
 source_lat, source_lon = 52.275284,20.938292
 dest_lat, dest_lon = 52.1204480,21.2464419
 
+# source_lat, source_lon = 52.169616, 21.202680
+# dest_lat, dest_lon = 52.136302, 21.017951
+
 file_name = "graph.ml"
-location = "Warsaw, Poland"
+location = "Mazowieckie, Poland"
 
 if not os.path.exists(file_name):
     print("Downloading graph")
@@ -37,10 +40,16 @@ def heur(n1, n2):
     n1y = G.nodes[n1]['y']
     n2x = G.nodes[n2]['x']
     n2y = G.nodes[n2]['y']
-    destx = dest_lon
-    desty = dest_lat
-    # distance from n1 to dest minus distance from n2 to dest
-    return ox.distance.great_circle(n1x, n1y, destx, desty) - ox.distance.great_circle(n2x, n2y, destx, desty)
+    return ox.distance.great_circle(n1x, n1y, n2x, n2y)
+
+def heur_travel_time(n1, n2):
+    n1x = G.nodes[n1]['x']
+    n1y = G.nodes[n1]['y']
+    n2x = G.nodes[n2]['x']
+    n2y = G.nodes[n2]['y']
+    # multiplied by Magic number - time needed to travel 1 meter at 50km/h
+    return ox.distance.great_circle(n1x, n1y, n2x, n2y)*0.072 
+   
 
 # find shortest path
 before = time.perf_counter()
@@ -59,30 +68,30 @@ print(f"Time taken: {after - before} seconds - A*, length")
 G = ox.routing.add_edge_speeds(G, fallback=30)
 G = ox.routing.add_edge_travel_times(G) # adds travel time (seconds) to each edge
 before = time.perf_counter()
-route_nodes3 = nx.astar_path(G, orig, dest, weight="travel_time", heuristic=heur)
+route_nodes3 = nx.astar_path(G, orig, dest, weight="travel_time", heuristic=heur_travel_time)
 after = time.perf_counter()
 print(f"Time taken: {after - before} seconds - A*, travel_time")
 
-# Find range
-reachable_nodes = nx.single_source_dijkstra_path_length(G, orig, weight="travel_time", cutoff=5*60)
-reachable_subgraph = G.subgraph(reachable_nodes.keys())
+# # Find range
+# reachable_nodes = nx.single_source_dijkstra_path_length(G, orig, weight="travel_time", cutoff=5*60)
+# reachable_subgraph = G.subgraph(reachable_nodes.keys())
 
-# plot the reachable subgraph
-fig, ax = plt.subplots()
+# # plot the reachable subgraph
+# fig, ax = plt.subplots()
 
-# Draw full graph in the background
-ox.plot_graph(G, ax=ax, show=False, close=False, edge_color="lightgray", bgcolor="white", node_size=0, edge_linewidth=0.5)
+# # Draw full graph in the background
+# ox.plot_graph(G, ax=ax, show=False, close=False, edge_color="lightgray", bgcolor="white", node_size=0, edge_linewidth=0.5)
 
-# Overlay the reachable subgraph
-ox.plot_graph(reachable_subgraph, ax=ax, show=False, close=False, edge_color="orange", node_size=0, edge_linewidth=1)
+# # Overlay the reachable subgraph
+# ox.plot_graph(reachable_subgraph, ax=ax, show=False, close=False, edge_color="orange", node_size=0, edge_linewidth=1)
 
-# Highlight the start node
-start_node_x, start_node_y = G.nodes[orig]["x"], G.nodes[orig]["y"]
-ax.plot(start_node_x, start_node_y, color="blue", marker="o", markersize=3)
+# # Highlight the start node
+# start_node_x, start_node_y = G.nodes[orig]["x"], G.nodes[orig]["y"]
+# ax.plot(start_node_x, start_node_y, color="blue", marker="o", markersize=3)
 
-ax.set_xlim((source_lon-0.1, source_lon+0.1))
-ax.set_ylim((source_lat-0.05, source_lat+0.05)) # random values but looks good with 5min cutoff :)
-plt.show()
+# ax.set_xlim((source_lon-0.1, source_lon+0.1))
+# ax.set_ylim((source_lat-0.05, source_lat+0.05)) # random values but looks good with 5min cutoff :)
+# plt.show()
 
 # Plot routes
 fig, ax1 = plt.subplots()
@@ -100,6 +109,9 @@ plt.show()
 gpd_route = ox.routing.route_to_gdf(G, route_nodes)
 gpd_route.to_file("route.geojson")
 
-# save to range.geojson
-gpd_range = ox.graph_to_gdfs(reachable_subgraph, nodes=False, edges=True)
-gpd_range.to_file("range.geojson")
+gpd_route = ox.routing.route_to_gdf(G, route_nodes3)
+gpd_route.to_file("route3.geojson")
+
+# # save to range.geojson
+# gpd_range = ox.graph_to_gdfs(reachable_subgraph, nodes=False, edges=True)
+# gpd_range.to_file("range.geojson")
